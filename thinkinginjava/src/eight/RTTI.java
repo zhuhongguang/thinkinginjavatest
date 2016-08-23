9dee391bc763a57a1798692036d62159189c70eb
Administrator <Administrator@TyrionZhu>
2016-08-23 14:10:54 +0800

    八和九

--- thinkinginjava/src/eight/RTTI.java
+++ thinkinginjava/src/eight/RTTI.java
@@ -1,5 +1,21 @@
 package eight;
-
+class Userful{
+	public void f(){}
+	public void g(){}
+}
+class MoreUserful extends Userful{
+	public void f(){}
+	public void g(){}
+	public void u(){}
+	public void v(){}
+	public void w(){}
+}
 public class RTTI {
-
+public static void main(String[] args) {
+	Userful[] x={new Userful(),new MoreUserful()};
+	x[0].f();
+	x[1].g();
+	((MoreUserful)x[1]).u();
+	((MoreUserful)x[0]).u();
+}
 }
---
+++ thinkinginjava/src/nine/NestingInterfaces.java
@@ -0,0 +1,106 @@
+package nine;
+
+class A {
+	interface B {
+		void f();
+	}
+
+	public class BImp implements B {
+		public void f() {
+		};
+	}
+
+	private class BImp2 implements B {
+		public void f() {
+		}
+	}
+
+	public interface C {
+		void f();
+	}
+
+	class CImp implements C {
+		public void f() {
+		}
+	}
+
+	private class CImp2 implements C {
+		public void f() {
+		}
+	}
+
+	private interface D {
+		void f();
+	}
+
+	private class DImp implements D {
+		public void f() {
+		}
+	}
+
+	public class DImp2 implements D {
+		public void f() {
+		}
+	}
+
+	public D getD() {
+		return new DImp2();
+	}
+
+	private D dRef;
+
+	public void receivdD(D d) {
+		dRef = d;
+		dRef.f();
+	}
+}
+
+interface E {
+	interface G {
+		void f();
+	}
+
+	public interface H {
+		void f();
+	}
+
+	void g();
+}
+
+public class NestingInterfaces {
+	public class BImp implements A.B {
+		public void f() {
+		}
+	}
+
+	class CImp implements A.C {
+		public void f() {
+		}
+	}
+
+	class EImp implements E {
+		public void g() {
+		}
+	}
+
+	class EGImp implements E.G {
+		public void f() {
+		}
+	}
+
+	class EImp2 implements E {
+		public void g() {
+		}
+
+		class EG implements E.G {
+			public void f() {
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		A a = new A();
+		A a2 = new A();
+		a2.receivdD(a.getD());
+	}
+}
