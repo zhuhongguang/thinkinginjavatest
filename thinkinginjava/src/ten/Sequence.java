0c9708d0a37666d5a5bb6fa04b11ec3c1d2ebfa7
Administrator <Administrator@TyrionZhu>
2016-08-24 08:08:28 +0800

    第十章

---
+++ thinkinginjava/src/ten/Sequence.java
@@ -0,0 +1,57 @@
+package ten;
+
+interface Selector {
+	boolean end();
+
+	Object current();
+
+	void next();
+}
+
+public class Sequence {
+	private int next = 0;
+	private Object[] items;
+
+	public Sequence(int size) {
+		items = new Object[size];
+	}
+
+	public void add(Object x) {
+		if (next < items.length)
+			items[next++] = x;
+	}
+
+	private class SequenceSelector implements Selector {
+		private int i = 0;
+
+		public boolean end() {
+			return i == items.length;
+		}
+
+		public Object current() {
+			return items[i];
+		}
+
+		public void next() {
+			if (i < items.length)
+				i++;
+		}
+	}
+
+	public Selector selector() {
+		return new SequenceSelector();
+	}
+
+	public static void main(String[] args) {
+		Sequence sequence = new Sequence(10);
+		for (int i = 0; i < 10; i++) {
+			sequence.add(Integer.toString(i));
+		}
+		Selector selector = sequence.selector();
+		while (!selector.end()) {
+			System.out.print(selector.current() + " ");
+		//	System.out.println(0>0);
+			selector.next();
+		}
+	}
+}
